bdf93bf62c9e0b73b6b6698b161be2dc402231da
will-feliciano <w.f.l@hotmail.com>
2019-02-12 18:09:34 -0200

    10-Conclusão

--- alurabank/app/js/models/imprimivel.js
+++ alurabank/app/js/models/imprimivel.js
@@ -1,13 +1,9 @@
 System.register([], function (exports_1, context_1) {
     "use strict";
-    var Imprimivel;
     var __moduleName = context_1 && context_1.id;
     return {
         setters: [],
         execute: function () {
-            Imprimivel = class Imprimivel {
-            };
-            exports_1("Imprimivel", Imprimivel);
         }
     };
 });
--- alurabank/app/ts/controllers/NegociacaoController.ts
+++ alurabank/app/ts/controllers/NegociacaoController.ts
@@ -57,32 +57,38 @@ export class NegociacaoController {
     }
 
     @throttle()
-    importData(){
+    async importData(){
 
-        this._service
-            .obterNegociacoes(res => {
-                if(res.ok){
-                    return res;
-                }
-                else{
-                    throw new Error(res.statusText);
-                }
-            })
-            .then(negociacoesParaImportar => {
-                const negociacoesJaImportadas = this._negociacoes.paraArray();
+        //this._service
+        try{
+            
+            const negociacoesParaImportar = await this._service
+                .obterNegociacoes(res => {
+                    if(res.ok){
+                        return res;
+                    }
+                    else{
+                        throw new Error(res.statusText);
+                    }
+                })
+                
+                //.then(negociacoesParaImportar => {
+            const negociacoesJaImportadas = this._negociacoes.paraArray();
 
-                negociacoesParaImportar
-                    .filter(negociacao => 
-                        !negociacoesJaImportadas.some(jaImportada =>
-                            negociacao.ehIgual(jaImportada)))
-                    .forEach(negociacao =>
-                    this._negociacoes.adiciona(negociacao));                        
-                    
-                this._negociacoesView.update(this._negociacoes);
-            })
-            .catch(err => {
-                this._mensagemView.update(err.message);
-            });        
+            negociacoesParaImportar
+                .filter(negociacao => 
+                    !negociacoesJaImportadas.some(jaImportada =>
+                        negociacao.ehIgual(jaImportada)))
+                .forEach(negociacao =>
+                this._negociacoes.adiciona(negociacao));                        
+                
+            this._negociacoesView.update(this._negociacoes);
+        //})
+        }
+        catch(err) {
+            this._mensagemView.update(err.message);
+        //});
+        }        
     }
 }
 
